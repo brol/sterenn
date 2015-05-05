f54c24545f018f289790f3dc95ce68522210430f
brol <brol31@gmail.com>
2015-05-05 18:48:31 +0200

    1.2

---
+++ _define.php
@@ -0,0 +1,20 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# Sterenn
+# Theme by Ludovic Toinel
+# Contributor : Pierre Van Glabeke
+# License: public domain
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+
+$this->registerModule(
+	/* Name */				  "Sterenn",
+	/* Description*/		"Le thème d'une étoile bretonne",
+	/* Author */			  "Ludovic Toinel, Pierre Van Glabeke",
+	/* Version */			  '1.2',
+	array(
+		'type'		=>		'theme'
+	)
+);
---
+++ _public.php
@@ -0,0 +1,12 @@
+<?php
+# ***** BEGIN LICENSE BLOCK *****
+#
+# Sterenn
+# Theme by Ludovic Toinel
+# Contributor : Pierre Van Glabeke
+# License: public domain
+#
+# ***** END LICENSE BLOCK *****
+if (!defined('DC_RC_PATH')) { return; }
+
+l10n::set(dirname(__FILE__).'/locales/'.$_lang.'/public');
